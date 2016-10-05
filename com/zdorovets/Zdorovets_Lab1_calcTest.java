3f33fcfbb4a31012ae0c78bbcc39bafd9c5e8b81
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-10-05 19:58:21 +0300

    v1.0

---
+++ com/zdorovets/Zdorovets_Lab1_calcTest.java
@@ -0,0 +1,35 @@
+package com.zdorovets;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class Zdorovets_Lab1_calcTest {
+
+	@Test
+	public void testAddWithZeroArgs() {
+		org.junit.Assert.assertEquals(0,Zdorovets_Lab1_calc.Add(""));
+	}
+	@Test
+	public void testAddWithOneArgument() {
+		org.junit.Assert.assertEquals(10,Zdorovets_Lab1_calc.Add("10"));
+	}
+		
+	@Test
+	public void testAddWithTwoArguments() {		
+		org.junit.Assert.assertEquals(34,Zdorovets_Lab1_calc.Add("16,18"));
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAddWithThreeArguments() {		
+		Zdorovets_Lab1_calc.Add("16,14,20");
+	}
+	
+	@Test(expected = NumberFormatException.class)
+	public void testNumberFormatException() {		
+		Zdorovets_Lab1_calc.Add("1,a");	
+	}
+	@Test(expected = RuntimeException.class)
+	public void testRuntimeException() {		
+		Zdorovets_Lab1_calc.Add(",");	
+	}
+}
