c1ad8c19d56774ca0a576025f88d3125b2e9dc1f
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-10-06 23:00:31 +0300

    v4 - Support different delimiters

--- com/zdorovets/Zdorovets_Lab1_calc.java
+++ com/zdorovets/Zdorovets_Lab1_calc.java
@@ -1,26 +1,27 @@
 package com.zdorovets;
 class Zdorovets_Lab1_calc {
+	public static int Add(String string)
+	{
+	String delimiter=",|\n";
+	String numbersWithoutDelimiter=string;
+	if (string.startsWith("//")) {
+        int delimiterIndex=string.indexOf("//")+2;
+        delimiter=string.substring(delimiterIndex,delimiterIndex+1);
+        numbersWithoutDelimiter=string.substring(string.indexOf("\n")+1);
+    }
+    return Add(numbersWithoutDelimiter, delimiter);
+	}
 	
-	public static int Add(String string) throws NumberFormatException,RuntimeException
-	{	
-		if(string.length()==0)
-		{			
-		    return 0;
-		}
-		else
-		{		
-		    String[] numbers=string.split(",|\n");
-		    if(numbers.length==0)
-    		    throw new RuntimeException("Error: invalid data.");
-		    else 
-		    {
-		    	int sum=0;
-		    	for(int i=0; i<numbers.length; i++)
-		    		sum+=Integer.parseInt(numbers[i]);
-		    	return sum;
+	public static int Add(String string,String delimiter) throws NumberFormatException,RuntimeException
+	{			
+			int sum=0; 
+		    String[] numbers=string.split(delimiter);
+		    for (String number : numbers) {
+		        if (!number.trim().isEmpty()) {
+		            sum += Integer.parseInt(number.trim());
+		        }
 		    }
-		    
-		}
+		    return sum;
 	}
 
 	public static void main(String[] args) {
