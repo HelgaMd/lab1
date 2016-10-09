1f2631e823529f3cb4e262cf59fb4b3a125283aa
VITTACH0 <zharikov.vitaliy@yandex.ru>
2016-10-09 13:35:28 +0300

    calculator_v0.2.2

--- calculator/src/calculator.java
+++ calculator/src/calculator.java
@@ -3,12 +3,17 @@
  */
 public class calculator {
     public static void main(String[] argv) {
-        calculate calc = new calculate();
+        calculate calc= new calculate();
 
-        System.out.println("Zharikov Vitaliy. Example:");
-        System.out.println("//;\\n,*\\n1;2,**4 = "+ calc.Add("//;\n,*\n1;2,**4"));
+        System.out.println("Zharikov Vitaliy. Examples:");
+        System.out.println("//;\\n,*\\n1;2,**4 = " + calc.Add("//;\n,*\n1;2,**4"));
         System.out.println("//;*\\n1;2***1000 = " + calc.Add("//;*\n1;2***1000"));
-        System.out.println("1,-2,-3\\n-6,7\\n = " + calc.Add("1,-2,-3\n-6,7\n"));
+        try {
+            calc.Add("1,-20,-3\n-14,7");
+        } catch (RuntimeException e) {
+            System.out.print(e.getMessage());
+            System.out.println();
+        }
         System.out.println("1,\\n = " + calc.Add("1,\n"));
         System.out.println("1\\n8 = " + calc.Add("1\n8"));
     }
--- calculator/src/calculatorTest.java
+++ calculator/src/calculatorTest.java
@@ -32,19 +32,39 @@ public class calculatorTest {
     }
 
     @Test
-    public void slash_slash_semicolon_dot_dot_backslash_n__8semicolon1000dotdot_1008result() {
+    public void AddTest_slashslashsemicolondotdotbackslashn_8semicolon1000dotdot_1008result() {
         calculate c = new calculate();
-        // Этот метод вызовет исключение, если результат нашего калькулятора будет отличен от 1008
+        // Этот метод вызовет исключение, если результат моего калькулятора будет отличен  1008
         assertEquals(c.Add("//;..\n8;1000.."), 1008);
+    }
+
+    @Test
+    public void AddTest_slashslashsemicolonbackslashn_8semicolonsemicolon1001dotdot_8result() {
+        calculate c = new calculate();
+        // Этот метод вызовет исключение, если результат моего калькулятора будет отличен  8
+        assertEquals(c.Add("//;\n8;;1001"),8);
+    }
+
+    @Test
+    public void AddTest_negative_elements() {
+        calculate calc = new calculate();
+        RuntimeException exception = null;
+        try {
+            calc.Add("1,-20,-3\n-14,7\n");
+        } catch (RuntimeException e) {
+            exception = e;
+        }
+        org.junit.Assert.assertNotNull(exception);
+        org.junit.Assert.assertEquals("Negatives not allowed: [-20, -3, -14]", exception.getMessage());
     }
 
     @Before
     public void testStarted() {
-        System.out.println("Test Started!");
+        System.out.println("Test Starting right now!");
     }
 
     @After
     public void testFinished() {
-        System.out.println("Test Finished");
+        System.out.println("Test Finished correctly");
     }
 }
