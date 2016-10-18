277eec501b1243f15fa3d037c804cde012049b32
ZharikovVitaliy <zharikov.vitaliy@yandex.ru>
2016-10-18 20:05:45 +0300

    calculator_v0.2.6

--- calculator/src/com/vittach/Calculate.java
+++ calculator/src/com/vittach/Calculate.java
@@ -7,44 +7,45 @@ import java.util.StringTokenizer;
  * Created by VITTACH on 05.10.2016.
  */
 public class Calculate {
-    public int Add(String fuckingCodacy) {
+    public int Add(String inputs) {
         int i= 2;
         int ceil;
         int agregate= 0;
+        String element = inputs;
         String agregateBuff = "";
         String tokenSymbols = "";
         boolean isNegative = false;
         ArrayList<Integer> negative = new ArrayList<>();
 
-        if (fuckingCodacy.length() > 0) {
-            if (fuckingCodacy.charAt(0) == '/'
-                    && fuckingCodacy.charAt(1) == '/') {
+        if (element.length() > 0) {
+            if (element.charAt(0) == '/'
+                    && element.charAt(1) == '/') {
                 while (true) {
-                    if (i + 1 < fuckingCodacy.length()) {
-                        if (fuckingCodacy.charAt(i)=='\n'
-                                && fuckingCodacy.charAt(i + 1) > '0'
-                                && fuckingCodacy.charAt(i + 1) < '9')
+                    if (i + 1 < element.length()) {
+                        if (element.charAt(i)=='\n'
+                                && element.charAt(i + 1) > '0'
+                                && element.charAt(i + 1) < '9')
                             break;
                     } else if(tokenSymbols.length()== 0)
                         return 0;
                     else
                         break;
-                    tokenSymbols += fuckingCodacy.charAt(i++);
+                    tokenSymbols += element.charAt(i++);
                 }
-                fuckingCodacy = fuckingCodacy.substring(i +1);
+                element = element.substring(i + 1);
             }
             else
                 tokenSymbols = "\n,";
 
             StringTokenizer strTok;
-            strTok = new StringTokenizer(fuckingCodacy, tokenSymbols);
+            strTok = new StringTokenizer(element, tokenSymbols);
             int countOfToken;
             countOfToken=strTok.countTokens();
 
             while (strTok.hasMoreElements()) {
-                String tokens=strTok.nextElement().toString();
+                String strTokEl=strTok.nextElement().toString();
 
-                ceil = Integer.parseInt(tokens);
+                ceil = Integer.parseInt(strTokEl);
                 if (ceil < 0) {
                     negative.add(ceil);
                     isNegative= true;
@@ -56,7 +57,7 @@ public class Calculate {
             }
 
             if (countOfToken == 1
-                    && agregateBuff.length() < fuckingCodacy.length())
+                    && agregateBuff.length() < element.length())
                 return 0;
 
             if (isNegative)
