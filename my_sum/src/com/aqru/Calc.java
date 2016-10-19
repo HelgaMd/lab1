9c2e25468c963f44152e6cc940007f85d0ddb825
Aqru <rkadriev@gmail.com>
2016-10-19 03:22:26 +0300

    lab 1

--- my_sum/src/com/aqru/Calc.java
+++ my_sum/src/com/aqru/Calc.java
@@ -15,7 +15,7 @@ class SubZero extends Exception {
 
 class Calc {
 
-    private void throw_negative_error(String numbers) throws SubZero{
+    private void throwNegativeError(String numbers) throws SubZero{
         Matcher m = Pattern.compile("(-\\d+)").matcher(numbers);
         if (m.find()){
             String str =  "Negatives not allowed. "+m.group(1)+" ";
@@ -25,30 +25,33 @@ class Calc {
         }
     }
 
-    public int add(String numbers) throws SubZero{
-        // if empty str - return 0
-        if (numbers.isEmpty()){
-            return 0;
-        }
-//        // if we have negative numbers...
-//        Matcher m = Pattern.compile("(-\\d+)").matcher(numbers);
-//        if (m.find()){
-//            String str =  "Negatives not allowed. "+m.group(1)+" ";
-//            while (m.find())
-//                str += m.group(1)+" ";
-//            throw new SubZero(str);
-//        }
-
-        //checking on negative numbers...
-        this.throw_negative_error(numbers);
-
+    private String getDelimiters(String numbers){
         // trying get delimiters
         Matcher m = Pattern.compile("\\[([^\\]]*)\\]").matcher(numbers);
         String delimiters = "";
         while (m.find())
             delimiters += m.group(1)+"|";
-        delimiters = delimiters.isEmpty() ? ",|\n" : delimiters.substring(0, delimiters.length()-1);
-        String str_num = ((m = Pattern.compile("//.*\\n(.*)").matcher(numbers)).find()) ? m.group(1) : numbers;
+        return delimiters.isEmpty() ? ",|\n" : delimiters.substring(0, delimiters.length()-1);
+    }
+
+    private String getNumbers(String numbers){
+        // get numbers
+        Matcher m = Pattern.compile("//.*\\n(.*)").matcher(numbers);
+        return (m.find()) ? m.group(1) : numbers;
+    }
+
+    public int add(String numbers) throws SubZero{
+        // if empty str - return 0
+        if (numbers.isEmpty()){
+            return 0;
+        }
+        //checking on negative numbers...
+        this.throwNegativeError(numbers);
+
+        // trying get delimiters
+        String delimiters = this.getDelimiters(numbers);
+        // getting numbers
+        String str_num = this.getNumbers(numbers);
 
         int result = 0;
         for (String s : Pattern.compile(delimiters).split(str_num)) {
