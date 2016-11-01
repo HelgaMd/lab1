9ddc2330aaa857f43663ee0a938bc7c4cb292aed
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-11-01 20:33:17 +0300

    Zdorovets (v9.6)
    выделил отдельные методы для работы с разделителями, выделил статические
    переменные класса

--- com/zdorovets/Zdorovets_Lab1_calc.java
+++ com/zdorovets/Zdorovets_Lab1_calc.java
@@ -19,45 +19,53 @@ class NegativeException extends Exception {
  * created on 04/10/16
  */
 class Zdorovets_Lab1_calc {
+	private static String delimiter;
+	private static String numbersWithoutDelimiter;
+	private static String input;
     /*
      * Метод add(String input_string)
-     * Нужен в случае со сложным(-и) разделителями для выделения разделителя и самого арифм. выражения
-     * Если не используются сложные разделители, то по умолчанию - "," и "\n"
-     * @param input_string входная строка, состоящая из разделителя(-ей) и выражения 
+     * Нужен в случае со сложным(-и) разделителями 
+     * для выделения разделителя и самого арифм. выражения
+     * Если не используются сложные разделители, 
+     * то по умолчанию - "," и "\n"
+     * @param input_string входная строка, 
+     * состоящая из разделителя(-ей) и выражения 
      * @return сумма двух чисел, либо исключение  
      */
     public static int add(String input_string) throws NumberFormatException, NegativeException {
-        String input = input_string;
-        String delimiter = ",|\n"; // разделитель по умолчанию
-        String numbersWithoutDelimiter = input;
-        if (input.startsWith("//["))
-            delimiter = ""; // если есть разделители со скобками, то начальный разделитель равен ""
-        int delimiterIndex,
-        i,
-        l;
-        while (input.startsWith("//[")) { //пока есть разделители
-            delimiterIndex = input.indexOf("//") + 3; // выделяется индекс, следующий за "//"
-            i = delimiterIndex;
-            l = 0;
-            if (!"".equals(delimiter))
-                delimiter = delimiter + "|"; // добавление символа, разделяющего разделители 
-            while (!input.substring(i, i + 1).equals("]")) { // пока не конец разделителя
-                delimiter = delimiter + input.substring(i, i + 1); // добавление к разделителю
-                												   // текущего символа
-                i++;
-                l++;
+        input = input_string; 
+        delimiter = ",|\n"; // разделитель по умолчанию
+        numbersWithoutDelimiter = input;
+        if (existDelimiters())
+            delimiter = ""; // если есть разделители со скобками, 
+                            //то начальный разделитель равен ""
+        int currentDelimIndex; // индекс символа, относящегося к разделителю, в строке
+        while (existDelimiters()) { //пока есть разделители
+        	// выделяется индекс, следующий за "//" и символом квадратной скобки
+            currentDelimIndex = getDelimIndexWithOffset(3);
+            if (isNotEmptyDelimiter())
+                delimiter = delimiter.concat("|"); // добавление символа, 
+                                                   // разделяющего разделители
+            // пока не конец разделителя
+            while (symbolBelongsToDelimiter(currentDelimIndex)) { 
+            	// добавление к разделителю текущего символа
+                delimiter = delimiter.concat(input.substring(currentDelimIndex, currentDelimIndex + 1)); 
+                currentDelimIndex++;
             }
-            input = input.substring(delimiterIndex + l + 1); // удаление очередного разделителя  
-                                                             // из анализируемой строки 
-            if (input.startsWith("[")) { // если есть еще разделители
-                input = "//" + input; // то прибавляем "//" для будущего анализа
-            } else numbersWithoutDelimiter = input.substring(input.indexOf("\n") + 1); // иначе выделяется  
-                                                                                       // строка без разделителя  
+            // удаление очередного разделителя из анализируемой строки
+            input = input.substring(currentDelimIndex + 1);  
+            if (existMoreDelimiters()) { // если есть еще разделители
+            	// то прибавляем "//" для будущего анализа
+                input = "//".concat(input); 
+            } else {
+            	// иначе выделяется строка без разделителя  
+            	numbersWithoutDelimiter = input.substring(input.indexOf("\n") + 1);  
+            }
         }
 
-        if (input.startsWith("//")) {
-            int delimIndex = input.indexOf("//") + 2;
-            delimiter = input.substring(delimIndex, delimIndex + 1);
+        if (existsShortDelimiter()) {
+            int delimIndex = getDelimIndexWithOffset(2);
+            delimiter = input.substring(delimIndex, delimIndex + 1); // разделитель состоит из одного символа
             numbersWithoutDelimiter = input.substring(input.indexOf("\n") + 1);
         }
         return add(numbersWithoutDelimiter, delimiter);
@@ -88,9 +96,43 @@ class Zdorovets_Lab1_calc {
         }
         return sum;
     }
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    /* 
+     * @return есть ли длинный разделитель в строке (начинающийся с "//[")
+     */
+    public static boolean existDelimiters(){
+    	return input.startsWith("//[");
+    }
+    /* 
+     * @return есть ли длинный разделитель в строке (начинающийся с "[", без учета символов "//")
+     */
+    public static boolean existMoreDelimiters(){
+    	return input.startsWith("[");
+    }
+    /* 
+     * @return есть ли короткий разделитель в строке (начинающиеся с "//")
+     */
+    public static boolean existsShortDelimiter(){
+    	return input.startsWith("//");
+    }
+    /* 
+     * @param offset смещение
+     * @return индекс разделителя со смещением
+     */
+    public static int getDelimIndexWithOffset(int offset){
+    	return input.indexOf("//") + offset;
+    }
+    /* 
+     * @return истина, если разделитель не равен пустой строке  
+     */
+    public static boolean isNotEmptyDelimiter(){
+    	return !"".equals(delimiter);
+    }
+    /* 
+     * @param currentDelimIndex текущий индекс разделителя
+     * @return истина, если нет символа завершения ("]")
+     */
+    public static boolean symbolBelongsToDelimiter(int currentDelimIndex){
+    	return !input.substring(currentDelimIndex, currentDelimIndex + 1).equals("]");
     }
 
 }
