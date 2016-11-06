7d7c66245935d3a341e9d009309bb6279cc0d603
evgenyzdorovets <jevgenijzdorovec@mail.ru>
2016-11-06 12:06:10 +0300

    Zdorovets (v9.7)
    выделены методы getDelimiter, setDelimiter, getNumberString,
    setNumberString, getInput, setInput. Метод add(String, String)
    переименован в calculateSum(String, String)

--- com/zdorovets/Zdorovets_Lab1_calc.java
+++ com/zdorovets/Zdorovets_Lab1_calc.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 /*
  * NegativeException class
  * @author Evgeny Zdorovets
- * @version 9.5
+ * @version 9.7
  * created on 04/10/16
  */
 class NegativeException extends Exception {
@@ -20,81 +20,44 @@ class NegativeException extends Exception {
  */
 class Zdorovets_Lab1_calc {
 	private static String delimiter;
-	private static String numbersWithoutDelimiter;
+	private static String numberString;
 	private static String input;
-    /*
-     * Метод add(String input_string)
-     * Нужен в случае со сложным(-и) разделителями 
-     * для выделения разделителя и самого арифм. выражения
-     * Если не используются сложные разделители, 
-     * то по умолчанию - "," и "\n"
-     * @param input_string входная строка, 
-     * состоящая из разделителя(-ей) и выражения 
-     * @return сумма двух чисел, либо исключение  
+	
+	/* 
+     * @return разделитель 
      */
-    public static int add(String input_string) throws NumberFormatException, NegativeException {
-        input = input_string; 
-        delimiter = ",|\n"; // разделитель по умолчанию
-        numbersWithoutDelimiter = input;
-        if (existDelimiters())
-            delimiter = ""; // если есть разделители со скобками, 
-                            //то начальный разделитель равен ""
-        int currentDelimIndex; // индекс символа, относящегося к разделителю, в строке
-        while (existDelimiters()) { //пока есть разделители
-        	// выделяется индекс, следующий за "//" и символом квадратной скобки
-            currentDelimIndex = getDelimIndexWithOffset(3);
-            if (isNotEmptyDelimiter())
-                delimiter = delimiter.concat("|"); // добавление символа, 
-                                                   // разделяющего разделители
-            // пока не конец разделителя
-            while (symbolBelongsToDelimiter(currentDelimIndex)) { 
-            	// добавление к разделителю текущего символа
-                delimiter = delimiter.concat(input.substring(currentDelimIndex, currentDelimIndex + 1)); 
-                currentDelimIndex++;
-            }
-            // удаление очередного разделителя из анализируемой строки
-            input = input.substring(currentDelimIndex + 1);  
-            if (existMoreDelimiters()) { // если есть еще разделители
-            	// то прибавляем "//" для будущего анализа
-                input = "//".concat(input); 
-            } else {
-            	// иначе выделяется строка без разделителя  
-            	numbersWithoutDelimiter = input.substring(input.indexOf("\n") + 1);  
-            }
-        }
-
-        if (existsShortDelimiter()) {
-            int delimIndex = getDelimIndexWithOffset(2);
-            delimiter = input.substring(delimIndex, delimIndex + 1); // разделитель состоит из одного символа
-            numbersWithoutDelimiter = input.substring(input.indexOf("\n") + 1);
-        }
-        return add(numbersWithoutDelimiter, delimiter);
+    public static String getDelimiter(){
+    	return delimiter;
     }
     /*
-     * Метод add(String input, String delimiter)
-     * Нужен для вычисления суммы
-     * @param input входная строка
-     * @param delimiter входной разделитель
-     * @return сумма двух чисел, либо исключение 
+     * устанавливает значение разделителя
      */
-    public static int add(String input, String delimiter) throws NumberFormatException, NegativeException {
-        int sum = 0;
-        String[] numbers = input.split(delimiter);
-        ArrayList < Integer > negativeList = new ArrayList < Integer > ();
-        int num;
-        for (String number: numbers) {
-            if (!number.trim().isEmpty()) {
-                num = Integer.parseInt(number.trim());
-                if (num < 0)
-                    negativeList.add(num);
-                else if (num <= 1000)
-                    sum += num;
-            }
-        }
-        if (negativeList.size() > 0) {
-            throw new NegativeException("Negatives not allowed: " + negativeList.toString());
-        }
-        return sum;
+    public static void setDelimiter(String val){
+    	delimiter = val;
+    }
+    /* 
+     * @return часть строки без разделителей
+     */
+    public static String getNumberString(){
+    	return numberString;
+    }
+    /*
+     * устанавливает значение части строки без разделителей
+     */
+    public static void setNumberString(String val){
+    	numberString = val;
+    }
+    /* 
+     * @return входная строка 
+     */
+    public static String getInput(){
+    	return input;
+    }
+    /*
+     * устанавливает значение входной строки
+     */
+    public static void setInput(String val){
+    	input = val;
     }
     /* 
      * @return есть ли длинный разделитель в строке (начинающийся с "//[")
@@ -133,6 +96,80 @@ class Zdorovets_Lab1_calc {
      */
     public static boolean symbolBelongsToDelimiter(int currentDelimIndex){
     	return !input.substring(currentDelimIndex, currentDelimIndex + 1).equals("]");
+    }  
+	
+    /*
+     * Метод add(String input_string)
+     * Нужен в случае со сложным(-и) разделителями 
+     * для выделения разделителя и самого арифм. выражения
+     * Если не используются сложные разделители, 
+     * то по умолчанию - "," и "\n"
+     * @param input_string входная строка, 
+     * состоящая из разделителя(-ей) и выражения 
+     * @return сумма двух чисел, либо исключение  
+     */
+    public static int add(String input_string) throws NumberFormatException, NegativeException {
+        setInput(input_string); 
+        setDelimiter(",|\n"); // разделитель по умолчанию
+        setNumberString(getInput());
+        if (existDelimiters())
+            setDelimiter(""); // если есть разделители со скобками, 
+                            //то начальный разделитель равен ""
+        int currentDelimIndex; // индекс символа, относящегося к разделителю, в строке
+        while (existDelimiters()) { //пока есть разделители
+        	// выделяется индекс, следующий за "//" и символом квадратной скобки
+            currentDelimIndex = getDelimIndexWithOffset(3);
+            if (isNotEmptyDelimiter())
+               setDelimiter(getDelimiter().concat("|")); // добавление символа, 
+                                                         // разделяющего разделители
+            // пока не конец разделителя
+            while (symbolBelongsToDelimiter(currentDelimIndex)) { 
+            	// добавление к разделителю текущего символа
+            	setDelimiter(getDelimiter().concat(getInput().substring(currentDelimIndex, currentDelimIndex + 1))); 
+                currentDelimIndex++;
+            }
+            // удаление очередного разделителя из анализируемой строки
+            setInput(getInput().substring(currentDelimIndex + 1));  
+            if (existMoreDelimiters()) { // если есть еще разделители
+            	// то прибавляем "//" для будущего анализа
+                setInput("//".concat(getInput())); 
+            } else {
+            	// иначе выделяется строка без разделителя  
+            	setNumberString(getInput().substring(getInput().indexOf("\n") + 1));  
+            }
+        }
+        if (existsShortDelimiter()) {
+            int delimIndex = getDelimIndexWithOffset(2);
+            setDelimiter(getInput().substring(delimIndex, delimIndex + 1)); // разделитель состоит из одного символа
+            setNumberString(getInput().substring(getInput().indexOf("\n") + 1));
+        }
+        return calculateSum(getNumberString(), getDelimiter());
+    }
+    /*
+     * Метод calculateSum(String input, String delimiter)
+     * Нужен для вычисления суммы
+     * @param input входная строка
+     * @param delimiter входной разделитель
+     * @return сумма двух чисел, либо исключение 
+     */
+    public static int calculateSum(String input, String delimiter) throws NumberFormatException, NegativeException {
+        int sum = 0;
+        String[] numbers = input.split(delimiter);
+        ArrayList < Integer > negativeList = new ArrayList < Integer > ();
+        int num;
+        for (String number: numbers) {
+            if (!number.trim().isEmpty()) {
+                num = Integer.parseInt(number.trim());
+                if (num < 0)
+                    negativeList.add(num);
+                else if (num <= 1000)
+                    sum += num;
+            }
+        }
+        if (negativeList.size() > 0) {
+            throw new NegativeException("Negatives not allowed: " + negativeList.toString());
+        }
+        return sum;
     }
 
 }
